71d024b853096389af49b1cb0d1d4a4263c6e23e
Joni Santos <jonisantos1996@hotmail.com>
2025-05-02 14:34:25 +0000

    [feat] pagination for posts

---
+++ src/front/js/component/Pagination.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+export const Pagination = ({ currentPage, setCurrentPage, totalPosts, postsPerPage, dark }) => {
+    const pages = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pages.push(i);
+    }
+    
+    return (
+        <div className="d-flex justify-content-center align-items-center mt-5 mb-3" style={{ width: "100wh" }}>
+            <nav aria-label="...">
+                <ul className="pagination">
+                    <li className={`page-item ${currentPage === 1 ? 'd-none' : ''}`}>
+                        <span className={`page-link ${dark ? 'bg-dark' : ''}`} onClick={() => setCurrentPage(currentPage - 1)}><i className="fa-solid fa-arrow-left"></i></span>
+                    </li>
+                    {pages.map((page, index) => {
+                        return <li className={`page-item ${currentPage === page ? 'active' : ''}`} key={index}><span className={`page-link ${dark ? 'bg-dark' : ''}`} onClick={() => setCurrentPage(page)}>{page}</span></li>
+                    })}
+                    <li className={`page-item ${currentPage === pages[pages.length - 1] ? 'd-none' : ''}`}>
+                        <span className={`page-link ${dark ? 'bg-dark' : ''}`} onClick={() => setCurrentPage(currentPage + 1)}><i className="fa-solid fa-arrow-right"></i></span>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+};
