60b8379a3931d3ff329806b4c62ef4d5cb04a614
Joni Santos <jonisantos1996@hotmail.com>
2025-07-01 21:39:17 +0000

    [fix] env

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     },
     plugins: [
         new Dotenv({
-            safe: true,
+            path: './src/front/.env',
             systemvars: true
         })
     ]
