e3daf25dedad72bf4a882d8dd73816a26d70c2b2
Joni Santos <jonisantos1996@hotmail.com>
2025-07-01 17:19:34 +0000

    [fix] countries arrays

--- src/front/js/pages/Countries.jsx
+++ src/front/js/pages/Countries.jsx
@@ -29,7 +29,7 @@ export const Countries = ({ dark }) => {
                 </div>
             </div>
             <div className="d-flex justify-content-center row row-cols-1 row-cols-md-2 g-4 mt-1 mb-5">
-                {countries.length !== 0 ? countries.map((item, index) => (
+                {Array.isArray(countries) && countries.length !== 0 ? countries.map((item, index) => (
                     <div className={`card mx-2 text-center ${dark ? 'bg-dark' : 'bg-grayish'}`} style={{ width: '22rem' }} key={index}>
                         <div className="card-body">
                             <h5 className="card-title">{item.name}</h5>
